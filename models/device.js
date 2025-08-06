ff3e26fce6696a19e42a674051e1b1c377959ad9
garv-cpu <balwanigarv2006@gmail.com>
2025-08-06 19:13:11 +0530

    omcmit new

--- models/device.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const deviceSchema = new mongoose.Schema({
-  deviceId: { type: String, required: true, unique: true },
-  status: { type: String, enum: ["locked", "unlocked"], default: "unlocked" },
-  lastSeen: { type: Date, default: Date.now },
-});
-
-export default mongoose.model("Device", deviceSchema);
